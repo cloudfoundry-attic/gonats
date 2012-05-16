85c8eacd0bc1dc17aea9fce9623fee4132f1a81b
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-05-16 14:04:24 -0700

    Prefix test helpers

--- reader_test.go
+++ reader_test.go
@@ -13,7 +13,7 @@ func createReader(payload string) *bufio.Reader {
 	return bufio.NewReader(sio)
 }
 
-func testMatch(t *testing.T, payload string, expected readObject) {
+func testReadMatch(t *testing.T, payload string, expected readObject) {
 	var rd *bufio.Reader
 	rd = createReader(payload)
 
@@ -25,7 +25,7 @@ func testMatch(t *testing.T, payload string, expected readObject) {
 	}
 }
 
-func testError(t *testing.T, payload string) {
+func testReadError(t *testing.T, payload string) {
 	var rd *bufio.Reader
 	rd = createReader(payload)
 
@@ -45,25 +45,25 @@ func TestReadMessage(t *testing.T) {
 		Payload:        []byte("some message"),
 	}
 
-	testMatch(t, "msg sub 1234 12\r\nsome message\r\n", expected)
+	testReadMatch(t, "msg sub 1234 12\r\nsome message\r\n", expected)
 }
 
 func TestReadMessageWithInvalidSubscriptionId(t *testing.T) {
-	testError(t, "msg sub xxxx 12\r\nsome message\r\n")
+	testReadError(t, "msg sub xxxx 12\r\nsome message\r\n")
 }
 
 func TestReadMessageWithInvalidByteCount(t *testing.T) {
-	testError(t, "msg sub 1234 xx\r\nsome message\r\n")
+	testReadError(t, "msg sub 1234 xx\r\nsome message\r\n")
 }
 
 func TestReadMessageWithEarlyEof(t *testing.T) {
-	testError(t, "msg sub 1234 12\r\nsome message\r")
+	testReadError(t, "msg sub 1234 12\r\nsome message\r")
 }
 
 func TestReadOk(t *testing.T) {
 	var expected = &readOk{}
 
-	testMatch(t, "+ok\r\n", expected)
+	testReadMatch(t, "+ok\r\n", expected)
 }
 
 func TestReadErrWithoutPayload(t *testing.T) {
@@ -71,7 +71,7 @@ func TestReadErrWithoutPayload(t *testing.T) {
 		Payload: nil,
 	}
 
-	testMatch(t, "-err\r\n", expected)
+	testReadMatch(t, "-err\r\n", expected)
 }
 
 func TestReadErrWithPayload(t *testing.T) {
@@ -79,7 +79,7 @@ func TestReadErrWithPayload(t *testing.T) {
 		Payload: []byte("foo"),
 	}
 
-	testMatch(t, "-err foo\r\n", expected)
+	testReadMatch(t, "-err foo\r\n", expected)
 }
 
 func TestReadErrWithPayloadWithWhiteSpace(t *testing.T) {
@@ -87,19 +87,19 @@ func TestReadErrWithPayloadWithWhiteSpace(t *testing.T) {
 		Payload: []byte("foo bar qux"),
 	}
 
-	testMatch(t, "-err foo bar qux\r\n", expected)
+	testReadMatch(t, "-err foo bar qux\r\n", expected)
 }
 
 func TestReadPing(t *testing.T) {
 	var expected = &readPing{}
 
-	testMatch(t, "ping\r\n", expected)
+	testReadMatch(t, "ping\r\n", expected)
 }
 
 func TestReadPong(t *testing.T) {
 	var expected = &readPong{}
 
-	testMatch(t, "pong\r\n", expected)
+	testReadMatch(t, "pong\r\n", expected)
 }
 
 func TestReadInfo(t *testing.T) {
@@ -107,5 +107,5 @@ func TestReadInfo(t *testing.T) {
 		ServerId: "some id",
 	}
 
-	testMatch(t, "info {\"server_id\":\"some id\" }\r\n", expected)
+	testReadMatch(t, "info {\"server_id\":\"some id\" }\r\n", expected)
 }
